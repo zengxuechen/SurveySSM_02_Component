1fbdb9f39c881db947813c77e20e75d662cc36d9
lixiaoxin <dnlixiaoxin@sina.com>
2018-04-05 19:17:58 +0800

    add query method

--- src/main/java/com/atguigu/survey/component/dao/i/TbDepartmentMapper.java
+++ src/main/java/com/atguigu/survey/component/dao/i/TbDepartmentMapper.java
@@ -1,9 +1,19 @@
 package com.atguigu.survey.component.dao.i;
 
+import com.atguigu.survey.entities.zhyq.TbDepartment;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 /**
  * Using IntelliJ IDEA.
  *
  * @author 李小鑫 at 2018/4/5 13:02
  */
 public interface TbDepartmentMapper {
+
+    List<TbDepartment> getDepartmentListByCompanyId(Integer companyId);
+
+    Integer saveDepartmentByCompanyId(@Param("companyId") Integer companyId , @Param("departmentName") String departmentName);
+
 }
--- src/main/java/com/atguigu/survey/component/service/m/DepartmentServiceImpl.java
+++ src/main/java/com/atguigu/survey/component/service/m/DepartmentServiceImpl.java
@@ -1,7 +1,13 @@
 package com.atguigu.survey.component.service.m;
 
+import com.atguigu.survey.component.dao.i.TbDepartmentMapper;
 import com.atguigu.survey.component.service.i.DepartmentService;
+import com.atguigu.survey.entities.zhyq.TbDepartment;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Using IntelliJ IDEA.
@@ -10,4 +16,17 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
+
+    @Autowired
+    TbDepartmentMapper departmentMapper;
+
+    public List<TbDepartment> getDepartmentListByCompanyId(Integer companyId) {
+        List<TbDepartment> result = departmentMapper.getDepartmentListByCompanyId(companyId);
+        return result;
+    }
+
+    public Integer saveDepartmentBuCompanyId(Integer companyId, String departmentName) {
+        Integer integer = departmentMapper.saveDepartmentByCompanyId(companyId, departmentName);
+        return integer;
+    }
 }
